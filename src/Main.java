fd15c779ffae444ebc574a1392a7b34bac7f61ea
hoanghai1310 <hoanghai.131096@gmail.com>
2022-05-09 14:01:48 +0700

    mainjava

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;         //Khai báo biến i kiểu interger vs giá trị là 10
+        float f = 20.5f;    //Khai báo biến f kiểu float vs giá trị là 20.5
+        double d = 20.5;    //Khai báo biến d kiểu double vs giá trị là 20.5
+        boolean b = true;   //Khai báo biến b kiểu boolean vs giá trị là true
+        char c = 'a';       //Khai báo biến c kiểu char vs giá trị là kí tự 'a'
+        String s = "Hà Nội";//Khai báo biến s kiểu String vs giá trị là chuỗi "Hà Nội"
+        System.out.println("i = " + i);//
+        System.out.println("f = " + f);//
+        System.out.println("d = " + d);//  In ra màn hình các giá trị trên vs lệnh System.out.printIn();
+        System.out.println("b = " + b);//
+        System.out.println("c = " + c);//
+        System.out.println("s = " + s);//
+    }
+}

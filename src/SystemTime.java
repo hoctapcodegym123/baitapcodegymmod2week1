04185c00f2c84183cd55a331ab7a7e21d0b54f96
hoanghai1310 <hoanghai.131096@gmail.com>
2022-05-09 11:49:23 +0700

    cmit

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class SystemTime {
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); //Lấy thời gian hiện tại
+        System.out.println("Now is :" + now);
+    }
 }
